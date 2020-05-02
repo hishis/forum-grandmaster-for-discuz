b174fbed29bcb28a7d7e1eceb0e6a7607f7933d1
admin <admin@example.com>
2020-05-02 03:09:09 +0000

    content update

--- main.js
+++ main.js
@@ -898,13 +898,13 @@ function main() {
                 `
                 <span class="img-upload"><em>🖼</em>伊姆古尔</span>
                 <span class="img-upload"><em>🖼</em>路过图床</span>
-                <span class="img-upload"><em>🖼</em>简单图床</span>
+                <span class="img-upload"><em>🖼</em>阿里图床</span>
                 <span class="img-upload"><em>🖼</em>虐恋图床</span>
                 `;
             let up_list = [
                 'https://imgur.com/upload',
                 'https://imgchr.com/',
-                'https://imghost.tech/',
+                'https://hao.su/tu/',
                 'https://sm.ms/',
             ];
             let up = spanButtonPlaceholder.getElementsByClassName('img-upload');
