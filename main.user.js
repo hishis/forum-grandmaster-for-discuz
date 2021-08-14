43e3044faf5343d2e023480233efba2eb6306993
admin <admin@example.com>
2021-08-14 09:14:58 +0000

    content update

--- main.user.js
+++ main.user.js
@@ -7,7 +7,7 @@
 // @name:zh-MO        論壇大師 – Discuz!
 // @name:zh-TW        論壇大師 – Discuz!
 // @namespace         Forum Grandmaster for Discuz!
-// @version           0.3.52
+// @version           0.3.53
 // @author            hostname
 // @description       🔊Beautify the interface, Remove ads, Enhance functions.
 // @description:en    🔊Beautify the interface, Remove ads, Enhance functions.
